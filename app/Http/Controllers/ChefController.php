0ad4a04b190c9d33d22c18c82e5f8720663a9081
Rama <rama.rose.ranneh@gmail.com>
2022-06-10 16:56:58 +0300

    edit the browsing chefs in chefController

--- app/Http/Controllers/ChefController.php
+++ app/Http/Controllers/ChefController.php
@@ -8,52 +8,38 @@ use App\Traits\DistanceCalculator;
 // a controller to control the chef model
 class ChefController extends Controller
 {
-
-    private function hideFromItem ($item) {
-        $item->makeHidden('chef_join_request_id');
-        $item->makeHidden('phone_number');
-        $item->makeHidden('email');
-        $item->makeHidden('birth_date');
-        $item->makeHidden('gender');
-        $item->makeHidden('location_id');
-        $item->makeHidden('delivery_starts_at');
-        $item->makeHidden('delivery_ends_at');
-        $item->makeHidden('max_meals_per_day');
-        $item->makeHidden('balance');
-        $item->makeHidden('certificate');
-        $item->makeHidden('approved_at');
-        $item->makeHidden('deleted_at');
-        return $item;
-    }
-    // a filter on the distance between the student and the chefs (get the nearest first)
-    // for browsing page
-    public function filterNearest()
-    {
-
-        // get the location of the student
-        $studentLocation = auth('user')->user()->location;
-        // get the distances between chefs and the student
-        // sort the chefs depending on the distances the lowest value first
-        //$chef->deleted_at !=null?$isblocked=true;
-
-        $sortedChefs = Chef::all()->map(fn($item) => $this->hideFromItem($item))->sortBy(function ($chef) use ($studentLocation) {
-            $chef->meals->rates_count;
-            $chef->meals->rating;
-            if ($studentLocation->id == 1)
-                return $chef->location->distance_to_first_location;
-            else if ($studentLocation->id == 2)
-                return $chef->location->distance_to_second_location;
-            else if ($studentLocation->id == 3)
-                return $chef->location->distance_to_third_location;
-        })->take(10)->values();
-        return $this->successResponse($sortedChefs);
+    /**
+     * hide not needed information in the browse page
+     * @param $chef
+     * @return mixed
+     */
+    private function hideFromItem ($chef) {
+        $chef->makeHidden('chef_join_request_id');
+        $chef->makeHidden('phone_number');
+        $chef->makeHidden('email');
+        $chef->makeHidden('birth_date');
+        $chef->makeHidden('gender');
+        $chef->makeHidden('location_id');
+        $chef->makeHidden('location');
+        $chef->makeHidden('meals');
+        $chef->makeHidden('delivery_starts_at');
+        $chef->makeHidden('delivery_ends_at');
+        $chef->makeHidden('max_meals_per_day');
+        $chef->makeHidden('balance');
+        $chef->makeHidden('certificate');
+        $chef->makeHidden('approved_at');
+        $chef->makeHidden('deleted_at');
+        $chef->makeHidden('orders');
+        return $chef;
     }
 
-
-        // a filter on the top-rated chefs to show in the browsing page
-        public function filterTopRated()
-        {
-            // following a method inspired by Bayesian probability
+    /**
+     * get the rating of a chef depending on his meals
+     * @param $chef
+     * @return array
+     */
+    private function getRate ($chef){
+        // following a method inspired by Bayesian probability
             // R refer to the "initial belief", 60th percentile (optimistic) or 40th percentile (pessimistic).
             // W is a fraction of number of rating, perhaps between C/20 and C/5 (depending on how noisy ratings are).
             // W = 0 is equivalent to using only the average of user ratings
@@ -65,14 +51,57 @@ class ChefController extends Controller
             // (R*W + sigma(n*rating)/(W+sigma(n))
             // depending on R and W if there is no rating => the item rate will be 25/10 = 2.5
 
-            $sortedChefs = Chef::all()->map(fn($item) => $this->hideFromItem($item))->sortByDesc(function ($chef) use ($R,$W){
+        [$ratingCount, $ratingValue]  =  $chef->meals->reduceSpread(function ($ratingCount,$ratingValue,$meal) use ($R,$W)  {
+            $ratingCount+=$meal->rates_count;
+            $ratingValue += $meal->rates_count*$meal->rating;
+            return [$ratingCount, $ratingValue] ;
+            },0,0);
+        return [($R*$W + ($ratingValue))/($W+$ratingCount),$ratingCount];
+}
+    /**
+     * a filter on the distance between the student and the chefs (get the nearest first)
+     * for browsing page
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function filterNearest()
+    {
+        // get the location of the student
+        $studentLocation = auth('user')->user()->location;
+        // get the distances between chefs and the student
+        // sort the chefs depending on the distances the lowest value first
+        $sortedChefs = Chef::with('location')->get()->map(function ($chef) {
+            $chef->chef_location = $chef->location->name;
+            [$ratingValue,$ratingCount]  = $this->getRate($chef);
+           $chef->chef_rate =$ratingValue;
+           $chef->chef_rate_count = $ratingCount;
+           return $this->hideFromItem($chef);
+        })->sortBy(function ($chef) use ($studentLocation) {
+            if ($studentLocation->id == 1)
+                return $chef->location->distance_to_first_location;
+            else if ($studentLocation->id == 2)
+                return $chef->location->distance_to_second_location;
+            else if ($studentLocation->id == 3)
+                return $chef->location->distance_to_third_location;
+        })->take(10)->values();
+        return $this->successResponse($sortedChefs);
+    }
+
+    /**
+     * a filter on the top-rated chefs to show in the browsing page
+     * @return \Illuminate\Http\JsonResponse
+     */
+        public function filterTopRated()
+        {
+            $sortedChefs = Chef::all()->map(function ($chef){
+                $chef->chef_location = $chef->location->name;
+                [$ratingValue,$ratingCount]  = $this->getRate($chef);
+                $chef->chef_rate =$ratingValue;
+                $chef->chef_rate_count = $ratingCount;
+                return $this->hideFromItem($chef);
+            })->sortByDesc(function ($chef){
                 // get the meals of each chef
-                [$ratingCount, $ratingValue]  =  $chef->meals->reduceSpread(function ($ratingCount,$ratingValue,$meal) use ($R,$W)  {
-                    $ratingCount+=$meal->rates_count;
-                    $ratingValue += $meal->rates_count*$meal->rating;
-                    return [$ratingCount, $ratingValue] ;
-                },0,0);
-                return ($R*$W + ($ratingValue))/($W+$ratingCount);
+                [$ratingValue,$ratingCount]  = $this->getRate($chef);
+                return $ratingValue ;
             })->values();
             return $this->successResponse($sortedChefs); // what to return ?
         }
@@ -80,14 +109,25 @@ class ChefController extends Controller
         {
             //count orders of each chef
             // sort the chefs depending on the count
-            $sortedChefs = Chef::all()->map(fn($item) => $this->hideFromItem($item))->sortByDesc(fn($chef) =>
-            count($chef->orders->where('status','delivered')))->values();
+            $sortedChefs = Chef::all()->map(function ($chef){
+                $chef->chef_location = $chef->location->name;
+                [$ratingValue,$ratingCount]  = $this->getRate($chef);
+                $chef->chef_rate =$ratingValue;
+                $chef->chef_rate_count = $ratingCount;
+                return $this->hideFromItem($chef);
+            })->sortByDesc(fn($chef) => count($chef->orders->where('status','delivered')))->values();
             return $this->successResponse($sortedChefs);
         }
 
         public function filterNewestChefs()
         {
-            $sortedChefs = Chef::all()->sortByDesc(fn($chef) => $chef->created_at)->values();
+            $sortedChefs = Chef::all()->map(function ($chef){
+                $chef->chef_location = $chef->location->name;
+                [$ratingValue,$ratingCount]  = $this->getRate($chef);
+                $chef->chef_rate =$ratingValue;
+                $chef->chef_rate_count = $ratingCount;
+                return $this->hideFromItem($chef);
+            })->sortByDesc(fn($chef) => $chef->created_at)->values();
             return $this->successResponse($sortedChefs);
         }
     }
